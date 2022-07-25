9ee3fcda47b0ff8df0554a0502251e78984ea0ae
Armen Marjinyan <armenmarjinyan@MacBook-Pro-Armen.local>
2022-07-25 16:59:20 +0400

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import React from 'react'
+class App extends React.Component{
+  state = {num:0}
+  countUp = () => {
+    this.setState(current => {
+      return {
+        num:current.num+1
+      }
+    })
+  }
+  render(){
+    const {num} = this.state
+    return <div> 
+      <h1>number - {num}</h1>
+      <button onClick={this.countUp}>Up</button>
     </div>
-  );
+  }
 }
-
-export default App;
+export default App
